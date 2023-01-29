e77aef1f35d20d797842b56c3f949e91a8729902
Mario Repas <mario.repas63@gmail.com>
2023-01-29 05:01:02 -0500

    debugged button issue and fetch data

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -1,22 +1,48 @@
-let searchFormEl = document.querySelector('#search-form');
+let searchFormEl = document.querySelector('#city-form');
 let cityInputEl = document.querySelector('#city');
 let currentWeatherEl = document.querySelector('#current-weather');
 let futureWeatherEl = document.querySelector('#future-weather');
+let searchBtn = document.querySelector('#searchBtn');
 
-cityInputEl.addEventListener('submit', formSubmitHandler);
-
-let formSubmitHandler = function (event) {
+let formSubmitHandler = function(event) {
     event.preventDefault();
+    let city = cityInputEl.value.trim();
+    console.log(city);
+    if (city) {
+        getCityWeather(city);
+        cityInputEl.value = "";
+    } else {
+        alert("Please enter a City");
+    }
+};
+// Fetching the weather data from the API - aync function allows the use of await
+let getCityWeather = async function(city) {
+    let apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=baa97083d89e9450d3519e9d509f7876";
 
-    var cityName = cityInputEl.value.trim();
-
-    if (cityName) {
-        getCurrentWeather(cityName);
+    try {
+        let response = await fetch(apiUrl);
+        if (response.ok) {
+            let data = await response.json();
+            let lat = data[0].lat;
+            let lon = data[0].lon;
+            console.log("Latitude: " + lat + ", Longitude: " + lon);
+            
+            // use the latitude and longitude to make an API call to get the current weather
+            let weatherApiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=baa97083d89e9450d3519e9d509f7876';
+            let weatherResponse = await fetch(weatherApiUrl);
+            let weatherData = await weatherResponse.json();
+            console.log(weatherData);
+            let currentTemperture = weatherData.main.temp;
+            let currentHumidity = weatherData.main.humidity;
+            let currentWindSpeed = weatherData.wind.speed;
+            let currentIcon = weatherData.weather[0].icon;
+            console.log(currentTemperture, currentHumidity, currentWindSpeed, currentIcon);
+        } else {
+            throw new Error("Error: " + response.statusText);
+        }
+    } catch (error) {
+        console.log(error);
     }
 };
 
-let getCurrentWeather = function(city){
-    
-    
-}
-
+searchFormEl.addEventListener('submit', formSubmitHandler);
